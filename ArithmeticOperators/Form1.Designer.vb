d87e60596ef526dfe10390a704eb3800da095542
nomvula <nzshabangu95@gmail.com>
2023-09-05 17:56:03 +0200

    Add project files.

---
+++ ArithmeticOperators/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtFirstNumber = New System.Windows.Forms.TextBox()
+        Me.txtSecondNumber = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(217, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "enter first number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(216, 154)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(160, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "enter second number"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(279, 245)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(148, 31)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtFirstNumber
+        '
+        Me.txtFirstNumber.Location = New System.Drawing.Point(413, 88)
+        Me.txtFirstNumber.Name = "txtFirstNumber"
+        Me.txtFirstNumber.Size = New System.Drawing.Size(100, 26)
+        Me.txtFirstNumber.TabIndex = 3
+        '
+        'txtSecondNumber
+        '
+        Me.txtSecondNumber.Location = New System.Drawing.Point(413, 154)
+        Me.txtSecondNumber.Name = "txtSecondNumber"
+        Me.txtSecondNumber.Size = New System.Drawing.Size(100, 26)
+        Me.txtSecondNumber.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtSecondNumber)
+        Me.Controls.Add(Me.txtFirstNumber)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtFirstNumber As TextBox
+    Friend WithEvents txtSecondNumber As TextBox
+End Class
---
+++ ArithmeticOperators/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim FirstNum As Integer
+        Dim SecondNum As Integer
+        Dim Answer As Double
+
+        FirstNum = txtFirstNumber.Text
+        SecondNum = txtSecondNumber.Text
+        'for addition
+        Answer = FirstNum + SecondNum
+        MsgBox(FirstNum & " + " & SecondNum & " = " & Answer)
+
+        'for subtraction
+        'Answer = FirstNum - SecondNum
+        MsgBox(FirstNum & " - " & SecondNum & " = " & Answer)
+
+        'for multiplication
+        Answer = FirstNum * SecondNum
+        MsgBox(FirstNum & " * " & SecondNum & " = " & Answer)
+
+
+        'for division
+        Answer = FirstNum / SecondNum
+        MsgBox(FirstNum & " / " & SecondNum & " = " & Answer)
+
+        'to the power
+        Answer = FirstNum ^ SecondNum
+        MsgBox(FirstNum & " ^ " & SecondNum & " = " & Answer)
+
+
+    End Sub
+End Class
